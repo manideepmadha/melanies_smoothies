ce341d395eeea2d7adb3f52d75d820f6bc9cd9e3
manideepmadha <43242240+manideepmadha@users.noreply.github.com>
2024-04-26 14:11:17 +0530

    Update streamlit_app.py
    sis to snis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,11 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+#from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+
+
+cntx = st.connection("snowflake")
+session = cntx.session()
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothe!")
